df399b89f5c95d012f92152c147dff13e6ffedea
Sherif <sherifookhamis@gmail.com>
2023-11-25 03:51:06 +0100

    Finished first page

---
+++ password_manager_gui/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace password_manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public bool get_passwordEntry ()
+        {
+            string passwordEntry = Enter_pwbox.Password;
+            if (passwordEntry == "Passwort")
+                return true;
+            return false;
+        }
+
+        public void enter_btn_clicked(object sender, RoutedEventArgs e)
+        {
+            if (get_passwordEntry() == false)
+                MessageBox.Show("Wrong password, try again!");
+        }
+    }
+}
