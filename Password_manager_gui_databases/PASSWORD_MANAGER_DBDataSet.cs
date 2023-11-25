8aab34ef4e9f2ab62b64964d749b43a851c84e6f
Sherif <sherifookhamis@gmail.com>
2023-11-25 11:15:59 +0100

    Added showAccounts() and a Listbox

---
+++ Password_manager_gui_databases/PASSWORD_MANAGER_DBDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace Password_manager_gui_databases
+{
+}
+
+namespace Password_manager_gui_databases
+{
+}namespace Password_manager_gui_databases {
+    
+    
+    public partial class PASSWORD_MANAGER_DBDataSet {
+    }
+}
+namespace Password_manager_gui_databases {
+    
+    
+    public partial class PASSWORD_MANAGER_DBDataSet {
+    }
+}
--- Password_manager_gui_databases/Window1.xaml.cs
+++ Password_manager_gui_databases/Window1.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Password_manager_gui_databases
 {
@@ -19,9 +22,30 @@ namespace Password_manager_gui_databases
     /// </summary>
     public partial class Window1 : Window
     {
+        SqlConnection sqlConnection;
         public Window1()
         {
             InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["Password_manager_gui_databases.Properties.Settings.PASSWORD_MANAGER_DBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            showAccounts();
+        }
+
+        private void showAccounts()
+        {
+            string query = "select * from Accounts";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+            using(sqlDataAdapter)
+            {
+                DataTable AccountTable = new DataTable();
+
+                sqlDataAdapter.Fill(AccountTable);
+
+                ListBox_Accounts.DisplayMemberPath = "Account";
+                ListBox_Accounts.ItemsSource = AccountTable.DefaultView;
+            }
+
         }
     }
 }
